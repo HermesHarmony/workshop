38af4fc18b248c543d5aff1670c3c99e45f8ba51
Jonas <35344671+HermesHarmony@users.noreply.github.com>
2026-03-05 11:27:07 +0100

    Typo korrigiert

--- src/js/templates/default.js
+++ src/js/templates/default.js
@@ -1,3 +1,3 @@
 // Das hier ist die Javascript Datei. Hier können Zusatzfunktionen für deine Seite implementiert werden. (Guide: 2.4)
 
-console.log('Die Javascript Datei wurde geladen. Sie tut derzeit aber nichts anders als diese Nachricht in die Konsole zu schreiben.');
+console.log('Die Javascript Datei wurde geladen. Sie tut derzeit aber nichts anderes als diese Nachricht in die Konsole zu schreiben.');
